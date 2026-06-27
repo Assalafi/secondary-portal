6dff2b23bf6cf822bf17027f9eff10e6ff341f94
Abubakar Abdullahi <abubakar@Assalafi-Macbook-Pro.local>
2026-06-27 20:56:13 +0100

    Add PDF facade import to fix PDF generation error

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use App\Models\Role;
+use PDF;
 
 class StudentController extends Controller
 {
